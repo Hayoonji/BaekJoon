6ea3ee548473aa6574269876ba15166867be33fd
Hayoonji <94597954+Hayoonji@users.noreply.github.com>
2023-03-21 23:37:01 +0900

    [Bronze V] Title: 고양이, Time: 120 ms, Memory: 13920 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args){
+        //Scanner scanner=new Scanner(System.in);
+
+        //long A=scanner.nextLong();
+		     
+		
+ 
+		System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+        
+        //scanner.close();
+    }
+}
