9c8b92acc5622b9dd93216bfe0fe4be3183e48e0
Hayoonji <94597954+Hayoonji@users.noreply.github.com>
2023-03-22 14:48:35 +0900

    [Bronze V] Title: 사분면 고르기, Time: 208 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        String str;
+        int a=scanner.nextInt();
+        str=scanner.nextLine();
+        int b=scanner.nextInt();
+        if((a>0) &&(b>0)){
+            System.out.println(1);
+        }else if((a<0) &&(b>0)){
+            System.out.println(2);
+        }else if((a<0) &&(b<0)){
+            System.out.println(3);
+        }else if((a>0) &&(b<0)){
+            System.out.println(4);
+        }
+        
+        scanner.close();
+
+    }
+}
