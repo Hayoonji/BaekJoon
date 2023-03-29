bb3a80223bdccc5408720bde6ddf31ddec51006c
Hayoonji <94597954+Hayoonji@users.noreply.github.com>
2023-03-29 16:44:38 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 200 ms, Memory: 17584 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        String str;
+        //int list[]=new int[100];
+        //int N=scanner.nextInt();//과목 개수
+        //int intArray[]=new int[N];//바구니
+        //str=scanner.nextLine(); 
+        str=scanner.next();
+        System.out.println(str.length());
+        
+
+        scanner.close();
+    }    
+}
