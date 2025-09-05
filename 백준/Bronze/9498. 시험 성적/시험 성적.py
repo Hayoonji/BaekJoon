f91eb478840af094da6d53196a3e5ce6355e6a08
Yoonji Ha <94597954+Hayoonji@users.noreply.github.com>
2025-09-05 13:43:55 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+a = input()
+
+if 100>=int(a) and int(a)>=90:
+  print("A")
+elif 89>=int(a) and int(a)>=80:
+  print("B")
+elif 79>=int(a) and int(a)>=70:
+  print("C")
+elif 69>=int(a) and int(a)>=60:
+  print("D")
+else:
+  print("F")
